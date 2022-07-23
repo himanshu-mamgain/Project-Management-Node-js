f99d765781c5f038f6215d2e7413a36fd7492661
Himanshu <himanshum50360@gmail.com>
2022-07-23 19:58:56 +0530

    remove delete route

--- routes/project.js
+++ routes/project.js
@@ -15,19 +15,4 @@ router.get("/projects", async (req, res) => {
     }
 });
 
-router.get("/:id", async (req, res) => {
-    try {
-        const foundProject = await Project.findById(req.params.id);
-        if(foundProject == null) {
-            res.redirect("/projects");
-        } else {
-            await foundProject.remove();
-            res.redirect("/projects");
-        }
-    } catch(err) {
-        console.log(err);
-        res.redirect("/projects");
-    }
-});
-
 module.exports = router;
